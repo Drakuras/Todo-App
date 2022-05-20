4c0ef1a47c2601f82b16d61e8f2a4dbbcf4d3f02
Drakuras <74796092+Drakuras@users.noreply.github.com>
2022-05-20 23:42:02 +0100

    Actually fixed the counter lol

--- script.js
+++ script.js
@@ -8,34 +8,39 @@ const addButton = document.querySelector(".add_button");
 const container = document.querySelector(".container");
 let counter = document.querySelector(".counter");
 const body = document.querySelector("body");
-const deleteButton = document.querySelector(".delete_button");
-const check = document.getElementsByClassName("checkbox");
+const deleteButton = document.getElementsByClassName("delete");
+
 const clearer = document.querySelector(".clearer");
 let i = 0;
 let todos = [];
+let arr = [];
 
 // Event listeners
 
 const submitHandler = (e) => {
   e.preventDefault();
   todoText.value == "" ? (todoText.value = "Empty") : todoText.value;
-
+  arr.push(deleteButton);
   container.insertAdjacentHTML(
     "beforeend",
     `<div draggable='true' class="todo_item border boxshadow">
-  <h1>${todoText.value}</h1>
+      <h1>${todoText.value}</h1>
   
-  <div class="options">
-    <input class='checkbox' type="checkbox" />
-    <button type='submit' class="delete_button"><span class="delete material-symbols-outlined">
-    delete
-    </span></button>
-  </div>
-  </div>`
+      <div class="options">
+        <input class='checkbox' type="checkbox" name='fruit' />
+        <button onclick='deleteHandler()' class="delete_button"><span class="delete material-symbols-outlined">
+        delete
+        </span></button>
+      </div>
+     </div>`
   );
   todoText.value = "";
 
   todos = [...container.children];
+
+  todos.map((el) => {
+    arr.push(el.children[1].children[0].checked);
+  });
 
   todos.map((el) => {
     el.children[1].lastElementChild.addEventListener("click", () => {
@@ -44,30 +49,12 @@ const submitHandler = (e) => {
   });
 
   counter.innerHTML = `<h1> ${container.childElementCount} Todo</br>${i} Completed</h1>`;
+  checkHandler();
 };
-let isItChecked = "d";
-document.addEventListener("click", (e) => {
-  let element = e.target;
-  let del = element.classList.contains("delete");
-  let ch = element.classList.contains("checkbox");
 
-  if (ch) {
-    if (element.checked === true) {
-      e.path[2].style.backgroundColor = "green";
-      e.path[2].style.transitionDuration = "200ms";
-      isItChecked = true;
-      i++;
-    } else if (element.checked === false) {
-      e.path[2].style.backgroundColor = "black";
-      e.path[2].style.transitionDuration = "200ms";
-      i--;
-      isItChecked = false;
-    }
-  } else if (del) {
-    if (isItChecked === true) {
-      i--;
-    }
-  }
+document.addEventListener("click", (e) => {
+  i = document.querySelectorAll(`input[type='checkbox']:checked`).length;
+
   counter.innerHTML = `<h1> ${container.childElementCount} Todo</br>${i} Completed</h1>`;
 });
 
@@ -76,3 +63,22 @@ clearer.addEventListener("click", () => {
   container.innerHTML = "";
   counter.innerHTML = `<h1> ${container.childElementCount} Todo</br>${i} Completed</h1>`;
 });
+
+const deleteHandler = () => {
+  i = document.querySelectorAll(`input[type='checkbox']:checked`).length;
+};
+
+const checkHandler = () => {
+  document.querySelectorAll(`input[type='checkbox']`).forEach((x) => {
+    x.onclick = () => {
+      i = document.querySelectorAll(`input[type='checkbox']:checked`).length;
+      if (x.checked) {
+        x.parentElement.parentElement.style.backgroundColor = "green";
+        x.parentElement.parentElement.style.transitionDuration = "300ms";
+      } else {
+        x.parentElement.parentElement.style.backgroundColor = "";
+        x.parentElement.parentElement.style.transitionDuration = "300ms";
+      }
+    };
+  });
+};
