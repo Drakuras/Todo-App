00ca0dd0817b90dacc7a97f9e3b0f288e050336f
Drakuras <74796092+Drakuras@users.noreply.github.com>
2022-05-21 14:54:04 +0100

    more cool stuff :+1:

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const container = document.querySelector(".container");
 let counter = document.querySelector(".counter");
 const body = document.querySelector("body");
 const deleteButton = document.getElementsByClassName("delete");
+const todoTitle = document.querySelector(".todo_title");
+const mainTitle = document.querySelector(".main_title");
+const addTitle = document.querySelector(".add_title");
 
 const clearer = document.querySelector(".clearer");
 let i = 0;
@@ -17,13 +20,29 @@ let arr = [];
 
 // Event listeners
 
+todoTitle.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTitle.click();
+  }
+});
+const titleHandler = (e) => {
+  e.preventDefault();
+
+  mainTitle.innerHTML =
+    todoTitle.value === ""
+      ? `<h1>Empty Title</h1>`
+      : `<h1>${todoTitle.value}</h1>`;
+
+  todoTitle.value = "";
+};
+
 const submitHandler = (e) => {
   e.preventDefault();
-  todoText.value == "" ? (todoText.value = "Empty") : todoText.value;
   arr.push(deleteButton);
   container.insertAdjacentHTML(
     "beforeend",
     `<div draggable='true' class="todo_item border boxshadow">
+
       <h1>${todoText.value}</h1>
   
       <div class="options">
@@ -48,20 +67,21 @@ const submitHandler = (e) => {
     });
   });
 
-  counter.innerHTML = `<h1> ${container.childElementCount} Todo</br>${i} Completed</h1>`;
+  counter.innerHTML = `<h1>Total: ${container.childElementCount}</br>Completed: ${i} </h1>`;
   checkHandler();
 };
 
 document.addEventListener("click", (e) => {
   i = document.querySelectorAll(`input[type='checkbox']:checked`).length;
 
-  counter.innerHTML = `<h1> ${container.childElementCount} Todo</br>${i} Completed</h1>`;
+  counter.innerHTML = `<h1>Total: ${container.childElementCount}</br>Completed: ${i} </h1>`;
 });
 
 clearer.addEventListener("click", () => {
   i *= 0;
   container.innerHTML = "";
-  counter.innerHTML = `<h1> ${container.childElementCount} Todo</br>${i} Completed</h1>`;
+  counter.innerHTML = `<h1>Total: ${container.childElementCount}</br>Completed: ${i} </h1>`;
+  mainTitle.innerHTML = `<h1>Empty Title</h1>`;
 });
 
 const deleteHandler = () => {
@@ -73,10 +93,10 @@ const checkHandler = () => {
     x.onclick = () => {
       i = document.querySelectorAll(`input[type='checkbox']:checked`).length;
       if (x.checked) {
-        x.parentElement.parentElement.style.backgroundColor = "green";
+        x.parentElement.parentElement.classList.add("coolbackground");
         x.parentElement.parentElement.style.transitionDuration = "300ms";
       } else {
-        x.parentElement.parentElement.style.backgroundColor = "";
+        x.parentElement.parentElement.classList.remove("coolbackground");
         x.parentElement.parentElement.style.transitionDuration = "300ms";
       }
     };
